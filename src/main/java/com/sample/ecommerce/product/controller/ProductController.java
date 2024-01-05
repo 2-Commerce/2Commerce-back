b218b80da9028aa01da56303717622a312961bef
lass9436 <lass9436@gmail.com>
2024-01-05 20:49:40 +0900

    feat: 가게 물품 확인 기능 추가

--- src/main/java/com/sample/ecommerce/product/controller/ProductController.java
+++ src/main/java/com/sample/ecommerce/product/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.sample.ecommerce.product.application.*;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,11 @@ public class ProductController {
     @PostMapping("/api/product")
     public ProductRegisterResponse registerProduct(ProductRegisterRequest productRegisterRequest){
         return new ProductRegisterResponse(productService.registerProduct(productRegisterRequest).getProductId());
+    }
+
+    @Operation(summary = "가게 물품을 확인한다.")
+    @GetMapping("/api/{storeId}/product")
+    public ProductInStoreGetResponse getProductInStore(@PathVariable("storeId") Long storeId){
+        return new ProductInStoreGetResponse(productService.getProductInStore(storeId));
     }
 }
