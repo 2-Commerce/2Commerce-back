918167348b852954952634706f5d5e9fffc8049e
lass9436 <lass9436@gmail.com>
2024-01-06 01:48:47 +0900

    feat: ProductUpdateRequest, ProductUpdateResponse 작성

---
+++ src/main/java/com/sample/ecommerce/product/application/ProductUpdateRequest.java
@@ -0,0 +1,33 @@
+package com.sample.ecommerce.product.application;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Schema(description = "물품 수정 요청")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductUpdateRequest {
+
+    @Schema(description = "물품 ID")
+    Long productId;
+
+    @Schema(description = "물품 이름")
+    String productName;
+
+    @Schema(description = "물품 분류")
+    String productCategory;
+
+    @Schema(description = "물품 수량")
+    Long productQuantity;
+
+    @Schema(description = "물품 가격")
+    Long productPrice;
+
+    @Schema(description = "물품 이미지 링크")
+    String productImageUrl;
+}
