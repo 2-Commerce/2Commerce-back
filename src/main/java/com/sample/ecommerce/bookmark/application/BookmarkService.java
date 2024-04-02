09c72e8999bd9234cbe8ac0f29da235b5dd03dff
lass <lass9436@gmail.com>
2024-04-03 00:34:24 +0900

    feat: bookmark 등록 수정

--- src/main/java/com/sample/ecommerce/bookmark/application/BookmarkService.java
+++ src/main/java/com/sample/ecommerce/bookmark/application/BookmarkService.java
@@ -2,6 +2,11 @@ package com.sample.ecommerce.bookmark.application;
 
 import com.sample.ecommerce.bookmark.domain.Bookmark;
 import com.sample.ecommerce.bookmark.domain.BookmarkRepository;
+import com.sample.ecommerce.product.domain.Product;
+import com.sample.ecommerce.product.domain.ProductRepository;
+import com.sample.ecommerce.user.domain.User;
+import com.sample.ecommerce.user.domain.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -10,18 +15,27 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class BookmarkService {
 
     BookmarkRepository bookmarkRepository;
+    UserRepository userRepository;
+    ProductRepository productRepository;
 
     public BookmarkDto registerBookmark(BookmarkRegisterRequest bookmarkRegisterRequest) {
-        Optional<Bookmark> bookmark = bookmarkRepository.findByUserIdAndProductId(bookmarkRegisterRequest.getUserId(), bookmarkRegisterRequest.getProductId());
-        if(bookmark.isEmpty()){
-            return bookmarkRepository.save(new Bookmark(bookmarkRegisterRequest)).toDto();
-        }else{
-            return bookmark.get().toDto();
-        }
+        User user = userRepository.findByUserId(bookmarkRegisterRequest.getUserId()).orElseThrow();
+        Product product = productRepository.findById(bookmarkRegisterRequest.getProductId()).orElseThrow();
+
+        Optional<Bookmark> findBookmark = bookmarkRepository.findByUserAndProduct(user, product);
+
+        if(findBookmark.isPresent()) throw new IllegalArgumentException("This Bookmark is already registered");
+
+        Bookmark bookmark = new Bookmark(bookmarkRegisterRequest);
+        bookmark.assignUser(user);
+        bookmark.assignProduct(product);
+
+        return bookmarkRepository.save(bookmark).toDto();
     }
 }
