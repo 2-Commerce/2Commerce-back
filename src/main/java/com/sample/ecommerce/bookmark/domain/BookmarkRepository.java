fa764b109c25a98c60d88ea62ad3135eeccd2a1e
lass <lass9436@gmail.com>
2024-04-03 00:34:13 +0900

    feat: bookmark repository 수정

--- src/main/java/com/sample/ecommerce/bookmark/domain/BookmarkRepository.java
+++ src/main/java/com/sample/ecommerce/bookmark/domain/BookmarkRepository.java
@@ -1,9 +1,11 @@
 package com.sample.ecommerce.bookmark.domain;
 
+import com.sample.ecommerce.product.domain.Product;
+import com.sample.ecommerce.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
-    Optional<Bookmark> findByUserIdAndProductId(String userId, Long productId);
+    Optional<Bookmark> findByUserAndProduct(User user, Product product);
 }
