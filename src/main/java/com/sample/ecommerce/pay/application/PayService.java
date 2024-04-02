90ad8a00595476c0b5ab4fba10160f7e471ebd97
lass <lass9436@gmail.com>
2024-04-03 03:30:32 +0900

    feat: 결제 내역 등록 수정

--- src/main/java/com/sample/ecommerce/pay/application/PayService.java
+++ src/main/java/com/sample/ecommerce/pay/application/PayService.java
@@ -3,22 +3,29 @@ package com.sample.ecommerce.pay.application;
 import com.sample.ecommerce.order.application.OrderService;
 import com.sample.ecommerce.pay.domain.PayTransaction;
 import com.sample.ecommerce.pay.domain.PayTransactionRepository;
+import com.sample.ecommerce.user.domain.User;
+import com.sample.ecommerce.user.domain.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class PayService {
 
     OrderService orderService;
 
+    UserRepository userRepository;
+
     PayTransactionRepository payTransactionRepository;
 
     public PayTransactionDto registerPay(PayRegisterRequest payRegisterRequest) throws IllegalAccessException {
-        final PayTransaction payTransaction = payTransactionRepository.save(new PayTransaction(payRegisterRequest, orderService.payOrder(payRegisterRequest)));
+        User user = userRepository.findByUserId(payRegisterRequest.getUserId()).orElseThrow();
+        final PayTransaction payTransaction = payTransactionRepository.save(new PayTransaction(payRegisterRequest, orderService.payOrder(payRegisterRequest), user));
         return payTransaction.toDto();
     }
 }
